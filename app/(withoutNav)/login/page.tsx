1366c6e25a183a0b8359d126b0cc8ccc9009f412
Michael McCann <mmccann0903@gmail.com>
2024-02-02 15:15:57 -0600

    redirect if already logged in

--- app/(withoutNav)/login/page.tsx
+++ app/(withoutNav)/login/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import LoginForm from "./_components/loginForm";
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default async function page() {
   const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
 
   return (
     <div>
--- app/(withoutNav)/register/page.tsx
+++ app/(withoutNav)/register/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import RegistrationForm from "./_components/registrationForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
   return <RegistrationForm />;
 }
