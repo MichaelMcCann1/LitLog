c1ed3313447d2514a9f4827e5415bd9b4bd1f70d
Michael McCann <mmccann0903@gmail.com>
2024-02-08 14:41:39 -0600

    add jest

---
+++ components/navbar/__tests__/logoutButton.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import LogoutButton from "../components/logoutButton";
+import userEvent from "@testing-library/user-event";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+
+describe("logoutButton", () => {
+  it("fires the callback function when clicked", async () => {
+    const mockCallback = jest.fn();
+    render(
+      <DropdownMenu open={true}>
+        <DropdownMenuContent>
+          <LogoutButton callback={mockCallback} />
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+
+    const button = screen.getByRole("button");
+    await userEvent.click(button);
+
+    expect(mockCallback).toHaveBeenCalled();
+  });
+});
